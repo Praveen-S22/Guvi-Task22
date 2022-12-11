b0d535591baff249df030b8db6e0c91f8dfc799e
Praveen S <96941471+Praveen-S22@users.noreply.github.com>
2022-12-11 20:58:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+React-Task02-ShopCart
+
+Rating according to given value,
+Disable add to cart button when added to card,
+Remove item from cart & enable add to cart
---
+++ src/CartItemsList.js
@@ -0,0 +1,10 @@
+
+
+export let CartItemsList=[
+    {id:1,itemName:"Cart-Item-1", imageUrl:"https://4.imimg.com/data4/IV/AO/MY-25476242/pure-leather-office-laptop-bag-for-men-in-brown-color-500x500.jpg", originalPrice:"$60.00",offer:false,currentPrice:"$60.00",starCount:3,showStar:true,inCart:false},
+    {id:2,itemName:"Cart-Item-2", imageUrl:"https://images-na.ssl-images-amazon.com/images/I/71%2BhdiB-RuL._SY450_.jpg", originalPrice:"$20.00",offer:true,currentPrice:"$18.00",starCount:5,showStar:true,inCart:false},
+    {id:3,itemName:"Cart-Item-3", imageUrl:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1596065164-best-laptop-bags-7-1596065157.jpg", originalPrice:"$50.00",offer:true,currentPrice:"$25.00",starCount:3,showStar:true,inCart:false},
+    {id:4,itemName:"Cart-Item-4", imageUrl:"https://img.themanual.com/image/themanual/peak-design-everyday-messenger-13l-laptop-bag-416x416.jpg", originalPrice:"$60.00",offer:false,currentPrice:"$60.00",starCount:3,showStar:true,inCart:false},
+    {id:5,itemName:"Cart-Item-5", imageUrl:"https://i.pinimg.com/originals/b0/5a/7a/b05a7a6f36d943bb7b40c3247ea2c4a7.jpg", originalPrice:"$40.00",offer:false,currentPrice:"$40.00",starCount:4,showStar:true,inCart:false},
+    {id:6,itemName:"Cart-Item-6", imageUrl:"https://images-na.ssl-images-amazon.com/images/I/81UENQ-QKAL._SX450_.jpg", originalPrice:"$280.00",offer:true,currentPrice:"$200.00",starCount:3,showStar:true,inCart:false},
+];
